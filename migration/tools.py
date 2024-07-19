5b5227ee8dececcd6a2055c0f51228c35e625cf5
Yoandy M <yoandym@gmail.com>
2024-07-19 15:27:37 -0400

    allow pretty print and pretty log

--- migration/tools.py
+++ migration/tools.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import os
+import json
 from colorama import Fore, Back, Style
 
-class PrettyPrint:
+class Pretty:
     """
     A utility class for pretty printing dictionaries and lists.
 
@@ -16,21 +18,48 @@ class PrettyPrint:
     OK_COLOR = Fore.GREEN
     FAILED_COLOR = Fore.RED
 
-    def __init__(self, data, state=OK_COLOR):
+    def log(data, file_path='./log.txt', overwrite=False, mode='w'):
         """
-        Initialize the PrettyPrint object.
+        Log data to a file with format.
+
+        Args:
+            data (dict or list): The data to log.
+            file_path (str): The path to the file where the log should be written.
+            mode (str): The file mode (e.g., 'w' for write, 'a' for append). Defaults to 'w'.
+            overwrite (bool): Whether to overwrite the file or not. Defaults to False.
+        """
+        if not overwrite and os.path.exists(file_path):
+            print(f'File {file_path} already exists. Set overwrite=True to overwrite it.')
+            return
+        
+        with open(file_path, mode) as file:
+            if type(data) == dict:
+                json.dump(data, file, indent=4)
+            elif type(data) == list:
+                for item in data:
+                    json.dump(item, file, indent=4)
+                    file.write('\n')
+            else:
+                file.write(data)
+            
+            file.write('\n')
+
+
+    def print(data, state=OK_COLOR):
+        """
+        Print the data.
 
         Args:
             data (dict or list): The data to print.
             state (str): The state color to use for printing. Defaults to OK_COLOR.
         """
         if type(data) == dict:
-            self._print_dict(data, state)
+            Pretty._print_dict(data, state)
         elif type(data) == list:
-            self._print_list(data, state)
+            Pretty._print_list(data, state)
         else:
             print(state + data)
-            print(Style.RESET_ALL + '\n')
+            print(Style.RESET_ALL)
 
     def _print_dict(self, data, state):
         """
