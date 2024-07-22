ec32bd634e1ba0c02141bdf6b1a15356aba638c9
Yoandy M <yoandym@gmail.com>
2024-07-22 18:21:13 -0400

    chore: minor bugs and improved docs

--- migration/tools.py
+++ migration/tools.py
@@ -3,6 +3,7 @@
 import os
 import json
 from colorama import Fore, Back, Style
+from pydantic import Union
 
 class Pretty:
     """
@@ -18,15 +19,16 @@ class Pretty:
     OK_COLOR = Fore.GREEN
     FAILED_COLOR = Fore.RED
 
-    def log(data, file_path='./log.txt', overwrite=False, mode='w'):
+    def log(data: Union[dict, list], file_path='./log.txt', overwrite=False, mode='w'):
         """
         Log data to a file with format.
 
         Args:
-            data (dict or list): The data to log.
+            data (Union[dict, list]): The data to log.
             file_path (str): The path to the file where the log should be written.
-            mode (str): The file mode (e.g., 'w' for write, 'a' for append). Defaults to 'w'.
             overwrite (bool): Whether to overwrite the file or not. Defaults to False.
+            mode (str): The file mode (e.g., 'w' for write, 'a' for append). Defaults to 'w'.
+
         """
         if not overwrite and os.path.exists(file_path):
             print(f'File {file_path} already exists. Set overwrite=True to overwrite it.')
@@ -45,41 +47,46 @@ class Pretty:
             file.write('\n')
 
 
-    def print(data, state=OK_COLOR):
+    def print(data: Union[dict, list], state=OK_COLOR):
         """
         Print the data.
 
         Args:
-            data (dict or list): The data to print.
+            data (Union[dict, list]): The data to print.
             state (str): The state color to use for printing. Defaults to OK_COLOR.
         """
         if type(data) == dict:
             Pretty._print_dict(data, state)
         elif type(data) == list:
             Pretty._print_list(data, state)
-        else:
+        elif data:
             print(state + data)
-            print(Style.RESET_ALL)
+        
+        print(Style.RESET_ALL)
 
-    def _print_dict(self, data, state):
+    def _print_dict(data: Union[dict, list], state):
         """
         Pretty print a dictionary.
 
         Args:
-            data (dict): The dictionary to print.
+            data (Union[dict, list]): The dictionary to print.
             state (str): The state color to use for printing.
         """
         for key, value in data.items():
             print(state + str(key) + ': ' + Style.RESET_ALL + str(value))
 
-    def _print_list(self, data, state):
+    def _print_list(data: Union[dict, list], state):
         """
         Pretty print a list of dictionaries.
 
         Args:
-            data (list): The list to print.
+            data (Union[dict, list]): The list to print.
             state (str): The state color to use for printing.
         """
         for item in data:
-            self._print_dict(item, state)
+            if isinstance(item, dict):
+                Pretty._print_dict(item, state)
+            else:
+                print(state + data)
+            
             print(Style.RESET_ALL + '\n')
